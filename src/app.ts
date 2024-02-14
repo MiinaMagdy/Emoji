29bb117dfc25a5cc7510d5b76dc74ccf00d962a3
Mina Magdy <mina.magdy.takawey@gmail.com>
2024-02-14 13:54:47 +0200

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,18 @@ function getRandomEmoji() {
   return emojies[Math.floor(Math.random() * emojies.length)];
 }
 
-app.get("/:id", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   const image = getRandomEmoji() + ".svg";
   const fileUrl = `https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/${image}`;
-  res.redirect(fileUrl);
+  fetch(fileUrl)
+    .then(async (response) => {
+      const data = Buffer.from(await response.arrayBuffer()).toString("base64");
+      res.send(`<img src="data:image/svg+xml;base64,${data}" />`);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send("Error");
+    });
 });
 
 app.listen(port, () => {
