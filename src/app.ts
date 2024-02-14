591c05d403256c10813043401eb50c6bfe22f0a8
Mina Magdy <mina.magdy.takawey@gmail.com>
2024-02-14 15:05:26 +0200

    logging the image url

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ function getRandomEmoji() {
 app.get("/emoji.svg", (req: Request, res: Response) => {
   const image = getRandomEmoji() + ".svg";
   const fileUrl = `https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/${image}`;
+  console.log(fileUrl);
   fetch(fileUrl)
     .then(async (response) => {
       const data = Buffer.from(await response.arrayBuffer()).toString();
